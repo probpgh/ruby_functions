0992565a21c5f129dc949ddd98f34822f349d072
PRob <PRob>
2017-01-30 19:42:24 -0500

    tried false condition for variables

--- ruby_function_add.rb
+++ ruby_function_add.rb
@@ -1,7 +1,7 @@
 def add(x_1, x_2) #two variables for the function "add"
 	if (x_1.is_a? Numeric) && (x_2.is_a? Numeric) #is_a? is limiting the variable to a single class (Numeric)
 		return (x_1 + x_2) #will try runs where the variables include a letter to see if false return
-
+							#Numeric is base class but can lead to more function for results
 	else
 
 		return false
--- test_rubyfunction_add.rb
+++ test_rubyfunction_add.rb
@@ -2,11 +2,17 @@ require "minitest/autorun"
 
 require_relative "ruby_function_add.rb"
 
-class Test_addition < Minitest::Test 
+class Test_addition < Minitest::Test #corrected an error wherein the first letter of the class must be capitalized ie "Test"
 
 	def Test_add_1_plus_1
 		results = add(1,1)#variables defined as 1 and 1
 		assert_equal(2, results)
 
 	end
+
+	def Test_add_1_plus_b
+		results = add(1,b) #variables defined as 1 and b
+		assert_equal(false, results)
+	end
+	
 end
