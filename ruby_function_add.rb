ddbdc58c03e8fd20453110d0587f6ebf3bb5bcb4
PRob <PRob>
2017-01-30 21:50:31 -0500

    add multiple parameters and changed to num

--- ruby_function_add.rb
+++ ruby_function_add.rb
@@ -1,10 +1,24 @@
-def add(x_1, x_2) #two variables for the function "add"
-	if (x_1.is_a? Numeric) && (x_2.is_a? Numeric) #is_a? is limiting the variable to a single class (Numeric)
-		return (x_1 + x_2) #will try runs where the variables include a letter to see if false return
+#def add(x_1, x_2) #two variables for the function "add"
+#	if (x_1.is_a? Numeric) && (x_2.is_a? Numeric) #is_a? is limiting the variable to a single class (Numeric)
+#		return (x_1 + x_2) #will try runs where the variables include a letter to see if false return
 							#Numeric is base class but can lead to more function for results
-	else
+#	else
 
-		return false
+#		return false
+
+#	end
+#end
+
+
+
+def add(*num) #accumulates variable amount of arguments and inserts them into the function as a list
+
+	sum = 0
+	num.each do |num| #sum started with zero and the acutal sum increases as each argument is added
+						#I changed all in example to "num" - Need to see if that is correct
+		sum += num #adds each variable to the sum
 
 	end
+
+	return sum
 end		
--- test_rubyfunction_add.rb
+++ test_rubyfunction_add.rb
@@ -4,19 +4,24 @@ require_relative "ruby_function_add.rb"
 
 class Test_addition < Minitest::Test #corrected an error wherein the first letter of the class must be capitalized ie "Test"
 
-	def Test_add_1_plus_1
-		results = add(1,1)#variables defined as 1 and 1
-		assert_equal(2, results)
+	#def Test_add_1_plus_1
+	#	results = add(1,1)#variables defined as 1 and 1
+	#	assert_equal(2, results)
+#
+#	end
 
-	end
+#	def Test_add_1_plus_b
+#		results = add(1,b) #variables defined as 1 and b
+#		assert_equal(false, results)
+#	end
 
-	def Test_add_1_plus_b
-		results = add(1,b) #variables defined as 1 and b
-		assert_equal(false, results)
-	end
+#	def Test_add_4_plus7
+#		results = add(4,7) #variables defined as 4 and 7
+#		assert_equal(11, results)
+#	end
 
-	def Test_add_4_plus7
-		results = add(4,7) #variables defined as 4 and 7
-		assert_equal(11, results)
+	def Test_add_5_parameters
+		results = add(2,4,6,8,10)
+		assert_equal(30, results)
 	end
 end
